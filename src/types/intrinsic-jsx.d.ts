5e1f8e2f51e90bc60d379b2878270a5f994342ed
seveibar <seveibar@gmail.com>
2023-04-08 23:08:34 -0700

    type progress and led circuit progress

--- src/types/intrinsic-jsx.d.ts
+++ src/types/intrinsic-jsx.d.ts
@@ -1,23 +1,31 @@
+import type * as B from "@tscircuit/builder"
+
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      resistor: any
+      resistor: Parameters<B.ResistorBuilder["setSourceProperties"]>[0]
       custom: any
-      capacitor: any
-      inductor: any
-      diode: any
-      bug: any
-      ground: any
-      powersource: any
-      group: any
-      trace: any
-      smtpad: any
-      port: any
-      ports: any
+      capacitor: Parameters<B.CapacitorBuilder["setSourceProperties"]>[0]
+      inductor: Parameters<B.InductorBuilder["setSourceProperties"]>[0]
+      diode: Parameters<B.DiodeBuilder["setSourceProperties"]>[0]
+      bug: Parameters<B.BugBuilder["setSourceProperties"]>[0]
+      ground: Parameters<B.GroundBuilder["setSourceProperties"]>[0]
+      powersource: Parameters<B.PowerSourceBuilder["setSourceProperties"]>[0]
+      group: Parameters<B.GroupBuilder["setSourceProperties"]>[0]
+      trace: Parameters<B.TraceBuilder["setSourceProperties"]>[0]
+      smtpad: Parameters<B.SMTPadBuilder["setSourceProperties"]>[0]
+      port: {
+        name: string
+        direction?: string
+        dir?: string
+        x: number | string
+        y: number | string
+      }
+      ports: Parameters<B.PortsBuilder["setSourceProperties"]>[0]
       footprint: any
       component: any
       platedhole: any
-      hole: any
+      hole: Partial<Omit<B.PCBHole, "type">>
       schematicdrawing: any
       box: any
       schematicbox: any
---
+++ tests/led-circuit.test.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import test from "ava"
+import { createRoot } from "lib/render"
+import { createProjectBuilder } from "@tscircuit/builder"
+import { logLayout } from "./utils/log-layout"
+
+const Resistor = () => (
+  <component>
+    <platedhole x={0} y={0} hole_diameter="1mm" outer_diameter="2mm" />
+    <platedhole x="0.2in" y={0} hole_diameter="1mm" outer_diameter="2mm" />
+    <box x="1.5mm" y={0} width="2.5mm" height="3mm" />
+    <line x1="0mm" y1="0mm" x2="3mm" y2="0mm" />
+    <port name="l" x="0mm" y="0mm" dir="left" />
+    <port name="r" x="3mm" y="0mm" dir="right" />
+  </component>
+)
+
+const ExampleCircuit = () => {
+  return (
+    <group>
+      <Resistor />
+    </group>
+  )
+}
+
+test("example led-circuit", async (t) => {
+  const pb = createProjectBuilder()
+  const result = await createRoot().render(<ExampleCircuit />, pb)
+  await logLayout("led circuit react-fiber", result)
+  t.pass()
+})
