03cf1cf680d780169baab2e6d3fa446ef246393c
seveibar <seveibar@gmail.com>
2023-09-09 14:44:45 -0700

    net alias test example

---
+++ tests/netalias.test.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import test from "ava"
+import { createRoot } from "lib/render"
+import { createProjectBuilder } from "@tscircuit/builder"
+import "types/intrinsic-jsx.d"
+
+test("render higher order component, <resistor /> and <custom />", async (t) => {
+  const pb = createProjectBuilder()
+  const result = await createRoot().render(
+    <group>
+      <netalias net="D0BUS" />
+    </group>,
+    pb
+  )
+  const port: any = result.find(
+    (e) => e.type === "source_port" && e.source_component_id === "net_alias_0"
+  )
+  console.log(port)
+  t.is(port.name, "D0BUS")
+})
