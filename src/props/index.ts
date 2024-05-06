5ccb620d25b8c4b76931f70a40a60c45bf946786
seveibar <seveibar@gmail.com>
2024-05-06 16:14:06 -0700

    wip

--- src/props/index.ts
+++ src/props/index.ts
@@ -16,6 +16,16 @@ export const relative_direction = z.enum([
   "right-to-left",
 ])
 
+export const explicit_pin_side_definition = z.object({
+  pins: z.array(z.number()),
+  direction: z.union([
+    z.literal("top-to-bottom"),
+    z.literal("left-to-right"),
+    z.literal("bottom-to-top"),
+    z.literal("right-to-left"),
+  ]),
+})
+
 export const common_layout_props = z.object({
   pcbX: distance.optional(),
   pcbY: distance.optional(),
@@ -24,7 +34,10 @@ export const common_layout_props = z.object({
   schY: distance.optional(),
   schRotation: rotation.optional(),
   pcbLayer: layer_ref.optional(),
-  footprint: z.string().optional(), // TODO pull in literals from @tscircuit/footprint
+
+  // TODO pull in literals from @tscircuit/footprint
+  // TODO footprint can be a string or react child
+  footprint: z.any().optional(),
 })
 
 export const supplier_props = z.object({
@@ -53,15 +66,29 @@ export const diode_props = common_component_props.extend({})
 
 export const led_props = common_component_props.extend({})
 
+export const board_props = z.object({
+  width: distance,
+  height: distance,
+  centerX: distance.optional().default(0),
+  centerY: distance.optional().default(0),
+  layout: z.any().optional(),
+})
+
 export const bug_props = common_component_props.extend({
-  // portLabels:
-  portArrangement: z.object({
-    pins: z.array(z.number()),
-    direction: z.union([
-      z.literal("top-to-bottom"),
-      z.literal("left-to-right"),
-      z.literal("bottom-to-top"),
-      z.literal("right-to-left"),
-    ]),
-  }),
+  portLabels: z.record(z.number(), z.string()),
+  portArrangement: z
+    .object({
+      leftSize: z.number().optional(),
+      topSize: z.number().optional(),
+      rightSize: z.number().optional(),
+      bottomSize: z.number().optional(),
+    })
+    .or(
+      z.object({
+        leftSide: explicit_pin_side_definition.optional(),
+        rightSide: explicit_pin_side_definition.optional(),
+        topSide: explicit_pin_side_definition.optional(),
+        bottomSide: explicit_pin_side_definition.optional(),
+      })
+    ),
 })
