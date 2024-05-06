b73c9b77a940318a8699daab7aed604f5050ffb4
seveibar <seveibar@gmail.com>
2024-05-06 14:25:31 -0700

    camel case and component hooks

---
+++ src/props/index.ts
@@ -0,0 +1,21 @@
+import { z } from "zod"
+import { distance, rotation } from "@tscircuit/soup"
+import { layer_ref } from "@tscircuit/builder"
+
+export const common_layout_props = z.object({
+  pcbX: distance.optional(),
+  pcbY: distance.optional(),
+  pcbRotation: rotation.optional(),
+  schX: distance.optional(),
+  schY: distance.optional(),
+  schRotation: rotation.optional(),
+  pcbLayer: layer_ref.optional(),
+  footprint: z.string().optional(), // TODO pull in literals from @tscircuit/footprint
+})
+
+export const supplier_props = z.object({})
+
+export const resistor_props = z.object({
+  name: z.string(),
+  resistance: z.string(),
+})
