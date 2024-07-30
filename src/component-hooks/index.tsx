e68ce81ec5f039a38f85f85f8e7939272d4223e3
Abhijit Roy <roya51788@gmail.com>
2024-07-30 22:15:20 +0530

    feat: add useBug alias with deprecation warning

--- src/component-hooks/index.tsx
+++ src/component-hooks/index.tsx
@@ -2,3 +2,8 @@ export * from "./create-use-component"
 export * from "./create-use-component-with-zod"
 export * from "./builtin-passives-use-components"
 export * from "./use-chip"
+
+/**
+ * @deprecated Use exports from "./use-chip" instead. This will be removed in a future version.
+ */
+export * as useBug from "./use-chip"
